26ce98e2458afb67839a155090633f4edc4add13
shahnozayadgar <yadgarova@kaist.ac.kr>
2023-08-18 16:21:32 +0300

    first commit

---
+++ components/UI/Button/Button.js
@@ -0,0 +1,14 @@
+import styles from'./Button.module.css';
+import React from 'react';
+
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+
+
+export default Button;
